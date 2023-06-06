4aceb9adb3f869d60a5c9271850859999ff0c933
alfonso <alfonso.sr.trabajo@gmail.com>
2023-06-06 19:57:42 +0200

    Add data retrievers
    + Create Base class for all data retrievers
    + Add classes for cascade height, hits per plane and radial distance

--- src/mingo/__init__.py
+++ src/mingo/__init__.py
@@ -1,6 +1,14 @@
 from .utils import reformat
 from .database import Database
-from .analysis import Hit_distribution
+from .analysis import (
+    Hit_distribution, Cascade_height, Plane_hits, Scattering
+)
 
-
-__all__ = ["reformat", "Database", "Hit_distribution"]
+__all__ = [
+    "reformat",
+    "Database",
+    "Hit_distribution",
+    "Cascade_height",
+    "Plane_hits",
+    "Scattering"
+]
