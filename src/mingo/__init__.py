38c70e77064e9e77e90671b619dce86e4dd40b22
carolFR13 <carolframa@gmail.com>
2023-08-25 01:33:10 +0200

    Errors fixed. New class available. + New class Matrix to get variable's values and statistical analysis + Classes Normaliced_matrix and Standardised_matrix for statistical analysis

--- src/mingo/__init__.py
+++ src/mingo/__init__.py
@@ -1,8 +1,8 @@
 from .utils import reformat
 from .database import Database, DBInput
 from .analysis import (
-    Hit_distribution, Shower_depth, Shower_waist, Average_time, Plane_hits,
-    Scattering, Plane_time, report, Matrix, Normaliced_matrix, Standardised_matrix
+    Hit_distribution, Shower_depth, Shower_waist, Plane_hits, Scattering, report,
+    Matrix, Normaliced_matrix, Standardised_matrix
 )
 
 __all__ = [
@@ -12,10 +12,8 @@ __all__ = [
     "Hit_distribution",
     "Shower_depth",
     "Shower_waist",
-    "Average_time",
     "Plane_hits",
     "Scattering",
-    "Plane_time",
     "report",
     "Matrix",
     "Normaliced_matrix",
